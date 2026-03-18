71f2fb3bb64ee144dc9353891b775dae600d97c5
Bhargav <bhargavpatel2022@gmail.com>
2026-03-18 11:44:44 -0400

    Add project files.

---
+++ ClinicAI.API/Program.cs
@@ -0,0 +1,28 @@
+using ClinicAI.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ClinicDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ClinicAI.Infrastructure/Persistence/ClinicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicAI.Infrastructure.Persistence
+{
+    public class ClinicDbContext :DbContext
+    {
+        public ClinicDbContext(DbContextOptions<ClinicDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<object> Dummy { get; set; }
+    }
+}
