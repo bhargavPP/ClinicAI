927cc1f8511b79aa81a850773b8d4cdb4de72a58
Bhargav <bhargavpatel2022@gmail.com>
2026-03-18 12:13:56 -0400

    solution update after project setup

--- ClinicAI.API/Program.cs
+++ ClinicAI.API/Program.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<ClinicDbContext>(options =>
                         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
