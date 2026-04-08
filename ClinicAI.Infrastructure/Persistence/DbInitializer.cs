e879500b5ac6cc1f17811406a0135cf0ec4e4ab7
Bhargav <bhargavpatel2022@gmail.com>
2026-04-08 12:16:58 -0400

    Added Doctor user in seeding

--- ClinicAI.Infrastructure/Persistence/DbInitializer.cs
+++ ClinicAI.Infrastructure/Persistence/DbInitializer.cs
@@ -12,42 +12,37 @@ namespace ClinicAI.Infrastructure.Persistence
             var context = scope.ServiceProvider.GetRequiredService<ClinicDbContext>();
 
             await context.Database.MigrateAsync();
-            if (await context.Users.AnyAsync(u => u.Role == "Doctor"))
-                return;
-            // ✅ Check if admin already exists
-            if (await context.Users.AnyAsync(u => u.Role == "Admin"))
-                return;
-
-            var admin = new User
+            if (!await context.Users.AnyAsync(u => u.Role == "Admin"))
             {
-                Id = Guid.NewGuid(),
-                Email = "admin@test.com" ,
-                FullName = "Admin User",
-                Phone = "1234567890",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin@123"),
-                Role = "Admin"
-            };
-            var doctor = new User
-            {
-                Id = Guid.NewGuid(),
-                Email = "doctor@test.com",
-                Phone = "1234567890",
-                FullName = "Doctor User",
-                PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin@123"),
-                Role = "Doctor"
+                var admin = new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "admin@test.com",
+                    FullName = "Admin User",
+                    Phone = "1234567890",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin@123"),
+                    Role = "Admin"
+                };
 
-            };       //var admin = new User
-            //{
-            //    Id = Guid.NewGuid(),
-            //    Email = "patient@test.com",
-            //    FullName = "patient User",
-            //    Phone = "1234567890",
-            //    PasswordHash = BCrypt.Net.BCrypt.HashPassword("Pat@123"),
-            //    Role = "patient"
-            //};
-            context.Users.Add(admin);
-            await context.SaveChangesAsync();
-            context.Users.Add(doctor);
+                context.Users.Add(admin);
+            }
+
+            // ✅ Create Doctor if not exists
+            if (!await context.Users.AnyAsync(u => u.Role == "Doctor"))
+            {
+                var doctor = new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "doctor@test.com",
+                    Phone = "1234567890",
+                    FullName = "Doctor User",
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword("Admin@123"),
+                    Role = "Doctor"
+                };
+
+                context.Users.Add(doctor);
+            }
+
             await context.SaveChangesAsync();
         }
     }
